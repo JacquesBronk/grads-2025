5f7f5e777bf2abb57a8e17cbacfdb9eaccaf3bad
JacquesB <jacquesb@agilebridge.co.za>
2025-01-28 07:38:02 +0200

    test

--- tests/ScoreMe/Program.cs
+++ tests/ScoreMe/Program.cs
@@ -2,7 +2,7 @@
 using System.Text.Json;
 
 const string apiPath = "https://retroscore.azurewebsites.net/updateScore";
-const string myName = "Jacques";
+const string myName = "Rebecca";
 
 string testPath = Path.Combine("tests", "E2ERetroShop", "TestResults.xml");
 
